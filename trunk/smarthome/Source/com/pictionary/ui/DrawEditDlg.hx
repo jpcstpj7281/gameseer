eea29624381b7cc478d09f8de958b13bf081e287
wills.astrol@gmail.com <wills.astrol@gmail.com@f19d378d-6d51-ac04-d330-ec84ea20e35e>
2012-05-15 09:27:47 +0000

    add DrawingDlgMgr.hx edit panel

--- trunk/smarthome/Source/com/pictionary/ui/DrawEditDlg.hx
+++ trunk/smarthome/Source/com/pictionary/ui/DrawEditDlg.hx
@@ -4,23 +4,19 @@ import nme.display.Sprite;
 import nme.display.Bitmap;
 import base.data.DataLoader;
 
-import nme.display.DisplayObject;
-
-import haxe.Timer;
 import com.pictionary.ui.DrawingDlgMgr;
 import base.ui.FixedDlg;
-import base.ui.CommDialogMgr;
-class EraseDlg extends FixedDlg{
-    public function new ( dm:CommDialogMgr ){
-        super(dm, new Bitmap( DataLoader.getInst().bms_.get("erase")));
-        x = 100;
+class DrawEditDlg extends FixedDlg{
+    public function new ( dm:DrawingDlgMgr){
+        super(dm, new Bitmap( DataLoader.getInst().bms_.get("btn_edit")));
+        x=20;
         y= nme.Lib.current.stage.stageHeight - 80;
     }
 
     public override function onMouseClick( ):Void{
         if ( _mgr.isAnimating() ) return;
         if ( Std.is( _mgr, DrawingDlgMgr)){
-            cast(_mgr, DrawingDlgMgr).eraseDrawing();
+            cast(_mgr, DrawingDlgMgr).toggleFixed();
         }
     }
 }
