cec10be067c803ec350c04d7e96d49b832732f8c
Wills.Astrol@gmail.com <Wills.Astrol@gmail.com@f19d378d-6d51-ac04-d330-ec84ea20e35e>
2012-07-27 19:21:20 +0000

    update

--- trunk/smarthome/Source/com/qbox/logic/Wnd.hx
+++ trunk/smarthome/Source/com/qbox/logic/Wnd.hx
@@ -4,16 +4,57 @@ package com.qbox.logic;
 
 class Wnd{
 
-    var _x:Int;
-    var _y:Int;
-    var _w:Int;
-    var _h:Int;
+    //var _x:Int;
+    //var _y:Int;
+    //var _w:Int;
+    //var _h:Int;
 
+    public var _screensWnd:Array<String>;
+
+    public var _virtualX:Int;
+    public var _virtualY:Int;
+    public var _virtualWidth:Int;
+    public var _virtualHeight:Int;
     public function new(){
-        _x = 0;
-        _y = 0;
-        _w = cast ScreenMgr.getInst()._width*0.6;
-        _h = cast ScreenMgr.getInst()._height*0.6;
+        //_x = 0;
+        //_y = 0;
+        //_w = cast ScreenMgr.getInst()._width*0.6;
+        //_h = cast ScreenMgr.getInst()._height*0.6;
+        _screensWnd = new Array<String>();
+    }
+
+    public function open(x:Int, y:Int, w:Int, h:Int){
+        _virtualX = x;
+        _virtualY = y;
+        _virtualWidth = w;
+        _virtualHeight = h;
+
+        for (i in ScreenMgr.getInst()._screens){ _screensWnd.push(i.openWnd( x,y,w,h)); }
+    }
+
+    public function move( x:Int, y:Int){
+        _virtualX = x;
+        _virtualY = y;
+        var ss = ScreenMgr.getInst()._screens;
+        for (i in 0...ss.length){ 
+            _screensWnd[i] = ss[i].moveWnd( _virtualX,_virtualY,_virtualWidth,_virtualHeight,_screensWnd[i]);
+        }
+    }
+    public function resize( w:Int, h:Int){
+
+        _virtualHeight = h;
+        _virtualWidth = w;
+        var ss = ScreenMgr.getInst()._screens;
+        for (i in 0...ss.length){ 
+            _screensWnd[i] = ss[i].resizeWnd( _virtualX,_virtualY,w,h,_screensWnd[i]);
+        }
+    }
+
+    public function close(){
+        var ss = ScreenMgr.getInst()._screens;
+        for (i in 0...ss.length){ 
+            ss[i].closeWnd( _screensWnd[i]);
+        }
     }
 
 }
--- trunk/smarthome/Source/com/qbox/ui/WndDlg.hx
+++ trunk/smarthome/Source/com/qbox/ui/WndDlg.hx
@@ -14,19 +14,26 @@ import com.qbox.logic.Wnd;
 
 class WndDlg extends CommDialog{
 
-    var _usrInput:EmbedTextField;
     var _iptext:EmbedTextField;
-    var _porttext:EmbedTextField;
-    var _portInput:EmbedTextField;
-    var _connBtn:EmbedTextField;
-    var _detail:EmbedTextField;
-    var _delete:EmbedTextField;
+    var _xt:EmbedTextField;
+    var _yt:EmbedTextField;
+    var _wt:EmbedTextField;
+    var _ht:EmbedTextField;
     var _wnd:Wnd;
 
     public function new ( dm:ListDialogMgr, c:Wnd){
         super(dm);
         _wnd = c;
         addChild( createElement());
+    }
+
+    override function show(){
+        _xt.text = "X:" +_wnd._virtualX;
+        _yt.text = "Y:"+_wnd._virtualY;
+        _wt.text = "W:"+_wnd._virtualWidth;
+        _ht.text = "H:"+_wnd._virtualHeight;
+        trace("test");
+        return super.show();
     }
 
     public function createElement():Sprite{
@@ -41,18 +48,47 @@ class WndDlg extends CommDialog{
         _iptext.width = 50;
         _iptext.height= 20;
 
-        _usrInput= new EmbedTextField();
-        _usrInput.text = "";
-        _usrInput.type = INPUT;
-        _usrInput.scaleX = 3;
-        _usrInput.scaleY = 3;
-        _usrInput.width = 60;
-        _usrInput.height= 16;
-        _usrInput.setBorder(true);
-        _usrInput.x= 60;
+        _xt= new EmbedTextField();
+        _xt.selectable = false;
+        _xt.text = "X:" +_wnd._virtualX;
+        _xt.scaleX = 3;
+        _xt.scaleY = 3;
+        _xt.width = 50;
+        _xt.height= 20;
+        _xt.x = 150;
+
+        _yt= new EmbedTextField();
+        _yt.selectable = false;
+        _yt.text = "Y:"+_wnd._virtualY;
+        _yt.scaleX = 3;
+        _yt.scaleY = 3;
+        _yt.width = 50;
+        _yt.height= 20;
+        _yt.x = 250;
+
+        _wt= new EmbedTextField();
+        _wt.selectable = false;
+        _wt.text = "W:"+_wnd._virtualWidth;
+        _wt.scaleX = 3;
+        _wt.scaleY = 3;
+        _wt.width = 50;
+        _wt.height= 20;
+        _wt.x = 350;
+
+        _ht= new EmbedTextField();
+        _ht.selectable = false;
+        _ht.text = "H:"+_wnd._virtualHeight;
+        _ht.scaleX = 3;
+        _ht.scaleY = 3;
+        _ht.width = 50;
+        _ht.height= 20;
+        _ht.x = 450;
 
         s.addChild( _iptext);
-        s.addChild( _usrInput);
+        s.addChild( _xt);
+        s.addChild( _yt);
+        s.addChild( _wt);
+        s.addChild( _ht);
         s.height = nme.Lib.current.stage.stageHeight/15;
         return s;
     }
