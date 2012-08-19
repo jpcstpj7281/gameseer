a67e7d81dbc424d5c8a457e1075000e7c6bb20e2
wills.astrol@gmail.com <wills.astrol@gmail.com@f19d378d-6d51-ac04-d330-ec84ea20e35e>
2012-08-19 07:27:51 +0000

    网络数据接收内容分段

--- trunk/qbox/msgHandler.cpp
+++ trunk/qbox/msgHandler.cpp
@@ -22,8 +22,7 @@ MsgHandler::~MsgHandler()
 
 }
 
-static void showData( const char* buf , bool isSend = false)
-{
+static void showData( const char* buf , bool isSend = false){
     unsigned char* rb = (unsigned char* )buf;
 
     if ( isSend ) printf("send str to client: ");
@@ -49,13 +48,13 @@ static void showData( const char* buf , bool isSend = false)
     printf( "\nlen: %d\n", len);
     printf( "\n");
 }
-
 void MsgHandler::netMsgInput(uint32_t connID,char* buff,uint32_t buffLen)
 {
 
 	MsgInfo msg;
 	NetMsgBody netMsg;
 
+    showData( buff);
 
 	if(buffLen <20)
 	{
@@ -63,102 +62,76 @@ void MsgHandler::netMsgInput(uint32_t connID,char* buff,uint32_t buffLen)
 		return;
 	}
 
-	uint32_t move = 0;
-    while ( move < buffLen )
-    {
-    	showData( buff + move);
+	uint32_t move =  sizeof(netMsg.msgHead);
+
+	memcpy((void*)&netMsg,(const void*)buff,sizeof(netMsg.msgHead));
+
+	memcpy((void*)&netMsg.msgType,(const void*)(buff+move),sizeof(netMsg.msgType));
+	move +=  sizeof(netMsg.msgType);
+
+	netMsg.msgData.append((const char*)buff+move,buffLen-move);
+
+	MsgBase msgBase;
+
+	msgBase.setMsgInfo(&msg);
+	msgBase.setNetMsgBody(&netMsg);
+	msgBase.UnpackMsg(msg);
 
 
-    	test_msg("netMsgInput\n");
+	for(map<uint32_t,CommModel*>::iterator it=m_model.begin();it!=m_model.end();it++)
+	{
+		it->second->onMsgReq(&msg,connID);
+	}
 
-        memcpy((void*)&netMsg,(const void*)(buff + move),sizeof(netMsg.msgHead));
-        move += sizeof(netMsg.msgHead);
-
-        test_msg("netMsgInput\n");
-        memcpy((void*)&netMsg.msgType,(const void*)(buff+move),sizeof(netMsg.msgType));
-        move +=  sizeof(netMsg.msgType);
-
-        test_msg("netMsgInput netMsg.msgType=%x,len=%d\n",netMsg.msgType,netMsg.msgHead.length);
-
-        uint32_t datalen = netMsg.msgHead.length - 12;//map data length!
-        //std::cout<<"test2:"<< buffLen <<std::endl;
-        //netMsg.msgData.append((const char*)buff+move,buffLen-move);
-        if ( datalen > 0 )
-        {
-            netMsg.msgData.append((const char*)buff+move, datalen);
-            move += datalen;
-        }
-
-        test_msg("netMsgInput\n");
-
-        MsgBase msgBase;
-        test_msg("netMsgInput\n");
-        msgBase.setMsgInfo(&msg);
-        test_msg("netMsgInput\n");
-        msgBase.setNetMsgBody(&netMsg);
-        test_msg("netMsgInput\n");
-       	msgBase.UnpackMsg(msg);
-
-
-        test_msg("sendto model size=%d\n",m_model.size());
-        for(map<uint32_t,CommModel*>::iterator it=m_model.begin();it!=m_model.end();it++)
-        {
-        	test_msg("sendto model %d\n",it->first);
-            it->second->onMsgReq(&msg,connID);
-        }
-        test_msg("onMsgReq OK\n");
-
-    }
 }
 
 
 bool MsgHandler::sendMsg(uint32_t connID,MsgInfo *pMsg)
 {
 
-    NetMsgBody netMsg;
+	NetMsgBody netMsg;
 
-    MsgBase msgBase;
+	MsgBase msgBase;
 
-    msgBase.setMsgInfo(pMsg);
-    msgBase.setNetMsgBody(&netMsg);
+	msgBase.setMsgInfo(pMsg);
+	msgBase.setNetMsgBody(&netMsg);
 
-    msgBase.packMsg(netMsg);
+	msgBase.packMsg(netMsg);
 
-    string sendBuff;
-    sendBuff.reserve( sizeof (NetMsgHead) + sizeof(uint32_t) + netMsg.msgData.size() ) ;
-    sendBuff.append((const char* )&netMsg,sizeof(NetMsgHead));
-    sendBuff.append((const char* )&netMsg.msgType,sizeof(uint32_t));
-    sendBuff.append((const char* )netMsg.msgData.c_str(),netMsg.msgData.size());
+	string sendBuff;
+	sendBuff.append((const char* )&netMsg,sizeof(NetMsgHead));
+	sendBuff.append((const char* )&netMsg.msgType,sizeof(uint32_t));
+	sendBuff.append((const char* )netMsg.msgData.c_str(),netMsg.msgData.size());
 
     showData( sendBuff.c_str() , true);
 
-    IServer::Instance()->SendNetMsg(connID,(char*)sendBuff.c_str(),sendBuff.size());
+	IServer::Instance()->SendNetMsg(connID,(char*)sendBuff.c_str(),sendBuff.size());
 
-    return true;
+	return true;
 
 }
 
 bool MsgHandler::broadcastMsg(MsgInfo *pMsg)
 {
 
-    NetMsgBody netMsg;
+	NetMsgBody netMsg;
 
-    MsgBase msgBase;
+	MsgBase msgBase;
 
-    msgBase.setMsgInfo(pMsg);
-    msgBase.setNetMsgBody(&netMsg);
+	msgBase.setMsgInfo(pMsg);
+	msgBase.setNetMsgBody(&netMsg);
 
-    msgBase.packMsg(netMsg);
+	msgBase.packMsg(netMsg);
 
-    string sendBuff;
-    sendBuff.append((const char* )&netMsg,sizeof(NetMsgHead));
-    sendBuff.append((const char* )&netMsg.msgType,sizeof(uint32_t));
-    sendBuff.append((const char* )netMsg.msgData.c_str(),netMsg.msgData.size());
+	string sendBuff;
+	sendBuff.append((const char* )&netMsg,sizeof(NetMsgHead));
+	sendBuff.append((const char* )&netMsg.msgType,sizeof(uint32_t));
+	sendBuff.append((const char* )netMsg.msgData.c_str(),netMsg.msgData.size());
 
 
-    IServer::Instance()->BroadcastMsg((char*)sendBuff.c_str(),sendBuff.size());
+	IServer::Instance()->BroadcastMsg((char*)sendBuff.c_str(),sendBuff.size());
 
-    return true;
+	return true;
 
 }
 
@@ -166,11 +139,11 @@ bool MsgHandler::broadcastMsg(MsgInfo *pMsg)
 
 MsgHandler* MsgHandler::Instance()
 {
-    if(m_instance == NULL)
-    {
-        m_instance = new MsgHandler();
-    }
-    return m_instance;
+	if(m_instance == NULL)
+	{
+		m_instance = new MsgHandler();
+	}
+	return m_instance;
 }
 
 
@@ -178,26 +151,26 @@ MsgHandler* MsgHandler::Instance()
 
 void MsgHandler::setModel(uint32_t modelType,CommModel *pModel)
 {
-    m_model.insert(make_pair(modelType,pModel));
+	m_model[modelType] = pModel;
 }
 
 
 
 CommModel* MsgHandler::getModule( uint32_t type )
 {
-    std::map< uint32_t, CommModel* >::iterator it = m_model.find(type);
-    if( it == m_model.end() )
-    {
-    	test_msg("can't find module instance. type=%u", type);
-        return NULL;
-    }
+	std::map< uint32_t, CommModel* >::iterator it = m_model.find(type);
+	if( it == m_model.end() )
+	{
+		//logth(Debug, "can't find module instance. type=%u", type);
+		return NULL;
+	}
 
-    if( it->second  )
-    {
-        return it->second;
-    }
-    else
-    {
-        return NULL;
-    }
+	if( it->second  )
+	{
+		return it->second;
+	}
+	else
+	{
+		return NULL;
+	}
 }
